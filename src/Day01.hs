7cc1d9cb33cfc3ec1f4e3ef0280e6019cea97171
Pablo Arias <pabloariasal@gmail.com>
2021-12-01 09:00:34 +0100

    solutions day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,16 @@
 module Day01 (solve) where
 
-part1 :: String -> String
-part1 _ = "Day 1 part 1"
+part1 :: [Int] -> Int
+part1 [] = 0
+part1 [_] = 0
+part1 (x:y:xs) = if y > x then 1 + part1 (y : xs) else part1(y : xs)
 
-part2 :: String -> String
-part2 s = "Day 1 part 2" ++ s
+part2 :: [Int] -> Int
+part2 = part1 . s
+    where s :: [Int] -> [Int]
+          s (x:y:z:xs) = (x + y + z) : s (y : z : xs)
+          s _ = []
 
 solve :: String -> IO ()
-solve input = putStrLn "--- Day 01 ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
+solve input = putStrLn "--- Day 01 ---" >> print (part1 $ p input) >> print (part2 $ p input)
+    where p = map read . lines
