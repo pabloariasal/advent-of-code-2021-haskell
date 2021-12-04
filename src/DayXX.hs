99c715b6296e1b61ca86a21a0c26b7fd6e977b7a
Pablo Arias <pabloariasal@gmail.com>
2021-12-04 11:30:13 +0100

    add template

---
+++ src/DayXX.hs
@@ -0,0 +1,10 @@
+module DayXX (solve) where
+
+part1 :: String -> String
+part1 _ = undefined
+
+part2 :: String -> String
+part2 _ = undefined
+
+solve :: String -> IO ()
+solve input = putStrLn "--- Day XX ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
