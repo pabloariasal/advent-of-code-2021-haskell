b503b32ddf47f375e2172ed1574383bd91384863
Pablo Arias <pabloariasal@gmail.com>
2021-12-05 15:18:55 +0100

    change template

--- src/DayXX.hs
+++ src/DayXX.hs
@@ -1,10 +1,10 @@
 module DayXX (solve) where
 
 part1 :: String -> String
-part1 _ = undefined
+part1 _ = "Not implemented"
 
 part2 :: String -> String
-part2 _ = undefined
+part2 _ = "Not implemented"
 
 solve :: String -> IO ()
 solve input = putStrLn "--- Day XX ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
