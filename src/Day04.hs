bca48ff14d43dbb05a030ebb20baeca71eda2b6b
Pablo Arias <pabloariasal@gmail.com>
2021-12-06 00:17:27 +0100

    day 04 part 2

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,16 +1,17 @@
 module Day04 (solve) where
 
-import Data.List.Split (splitOn)
 import Data.List (transpose)
+import Data.List.Split (splitOn)
 
 type Board = [[Int]]
 
 part1 :: String -> String
-part1 s =
-  let parts = lines s
-      nums = parseList (head parts)
-      boards = parseBoards (tail parts)
-   in show $ play nums boards
+part1 = show . uncurry play . parse
+
+parse :: String -> ([Int], [Board])
+parse s = (parseList (head parts), parseBoards (tail parts))
+  where
+    parts = lines s
 
 parseList :: String -> [Int]
 parseList = map read . splitOn ","
@@ -33,17 +34,33 @@ play (x : xs) bs = case filter isFinished updated of
 markNumber :: Int -> Board -> Board
 markNumber n = map (map f)
   where
-    f i = if i == n then -1 else i 
+    f i = if i == n then -1 else i
 
 isFinished :: Board -> Bool
-isFinished b = any (all (==(-1))) b || any (all (==(-1))) (transpose b)
+isFinished b = any (all isMarked) b || any (all isMarked) (transpose b)
+  where
+    isMarked = (== (-1))
 
 computeScore :: Int -> Board -> Int
 computeScore n b = n * sum (map rs b)
-  where rs = foldr (\e a -> if e == -1 then a else e + a) 0
+  where
+    rs = foldr (\e a -> if e == -1 then a else e + a) 0
 
 part2 :: String -> String
-part2 _ = "Not implemented"
+part2 = show . uncurry play2 . parse
+
+play2 :: [Int] -> [Board] -> Int
+play2 = f 0
+  where
+    f s _ [] = s
+    f s [] _ = s
+    f s (x : xs) bs = case won of
+      [] -> f s xs notwon
+      (b : _) -> f (computeScore x b) xs notwon
+      where
+        won = filter isFinished updated
+        notwon = filter (not . isFinished) updated
+        updated = map (markNumber x) bs
 
 solve :: String -> IO ()
 solve input = putStrLn "--- Day 04 ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
