874b8596f6b0d196abcd76f8ccb9b431d1951476
Pablo Arias <pabloariasal@gmail.com>
2021-12-09 19:12:57 +0100

    solve day 7

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import qualified Day03 (solve)
 import qualified Day04 (solve)
 import qualified Day05 (solve)
 import qualified Day06 (solve)
+import qualified Day07 (solve)
 import System.Environment
 import System.Exit
 
@@ -17,7 +18,8 @@ solutions = Map.fromList [("01", Day01.solve),
                           ("03", Day03.solve),
                           ("04", Day04.solve),
                           ("05", Day05.solve),
-                          ("06", Day06.solve)
+                          ("06", Day06.solve),
+                          ("07", Day07.solve)
                          ]
 
 solveSingle :: String -> IO ()
---
+++ src/Day07.hs
@@ -0,0 +1,30 @@
+module Day07 (solve) where
+
+import Data.List.Split (splitOn)
+
+-- this is saddly a brute force solution, as I couldn't really think of anything better
+-- surfing the internet however made me realize that this is just the mean and median😅
+part1 :: String -> String
+part1 i = show $ minimum (map m input)
+  where input = parse i
+        m = flip computeFuelCost input
+
+part2 :: String -> String
+part2 i = show $ minimum (map m input)
+  where input = parse i
+        m = flip computeFuelCost2 input
+
+parse :: String -> [Int]
+parse i = map read (splitOn "," i)
+
+computeFuelCost :: Int -> [Int] -> Int
+computeFuelCost n = sum . differences n
+
+computeFuelCost2 :: Int -> [Int] -> Int
+computeFuelCost2 n l = sum $ [ sum [1..d] | d <- differences n l]
+
+differences :: Int -> [Int] -> [Int]
+differences n = map (abs . (-) n)
+
+solve :: String -> IO ()
+solve input = putStrLn "--- Day 07 ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
