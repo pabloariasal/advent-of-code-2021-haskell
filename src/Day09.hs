ecb8fc8a29acc6d52d577096e24cf23ccd01f051
Pablo Arias <pabloariasal@gmail.com>
2021-12-12 16:56:51 +0100

    day 09 part 2

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,6 +2,7 @@ module Day09 (solve, part1, part2) where
 
 import Data.Char (digitToInt)
 import Data.Maybe (mapMaybe)
+import Data.List (sort)
 import qualified Data.Vector as V
 
 type Coord = (Int, Int)
@@ -9,16 +10,16 @@ type Coord = (Int, Int)
 data Mat = Mat {values :: V.Vector Int, width :: Int, height :: Int} deriving (Eq, Show)
 
 part1 :: String -> String
-part1 s = show $ sum (map ((+1) . val m ) minima)
+part1 s = show $ sum (map ((+ 1) . val m) (getMinima m))
   where
-    minima = filter (isLocalMin m) allCoords
-    allCoords = [(r, c) | r <- [0 .. (h -1)], c <- [0 .. (w -1)]]
-    h = height m
-    w = width m
     m = parse s
 
-part2 :: String -> String
-part2 _ = "Not implemented"
+getMinima :: Mat -> [Coord]
+getMinima m = filter (isLocalMin m) allCoords
+  where
+    allCoords = [(r, c) | r <- [0 .. (h - 1)], c <- [0 .. (w - 1)]]
+    h = height m
+    w = width m
 
 parse :: String -> Mat
 parse s = Mat d width height
@@ -48,6 +49,19 @@ val :: Mat -> Coord -> Int
 val m (r, c) = values m V.! i
   where
     i = r * width m + c
+
+part2 :: String -> String
+part2 s = show . product . take 3 . reverse . sort . map length $ basins
+  where
+    basins = map (basin m []) minima
+    minima = getMinima m
+    m = parse s
+
+basin :: Mat -> [ Coord ] -> Coord -> [Coord]
+basin m v c =
+  if (c `notElem` v) && val m c < 9
+    then foldl (basin m) (c : v) (neighboringCells m c)
+    else v
 
 solve :: String -> IO ()
 solve input = putStrLn "--- Day 09 ---" >> putStrLn (part1 input) >> putStrLn (part2 input)
